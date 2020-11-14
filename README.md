ec7a0d08859670611b9f9677da911c952e7d1833
Felix Yeung <FelixYeung@Felixs-MacBook-Pro.local>
2020-11-14 17:21:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Feli URL Shortener
+Shortens URL. 
+
+Chrome Extension available https://github.com/felixyeungdev/url.feli.page-chrome-extension
+
+## Scripts
+`dateFormat.min.js` from https://github.com/phstc/jquery-dateFormat
+`clipboard.min.js` from https://clipboardjs.com/
---
+++ public/index.js
@@ -0,0 +1,107 @@
+let inputField = document.querySelector("input#input-url");
+let outputField = document.querySelector("input#output-url");
+let convertButton = document.querySelector("button#convert");
+let copyButton = document.querySelector("button#copy");
+let historySection = document.querySelector("#history");
+let historySectionList = historySection.querySelector(".list");
+
+var converting = false;
+
+function createHistoryItem(item) {
+    const itemElement = document.createElement("div");
+    itemElement.classList.add("history-item");
+
+    const time = document.createElement("p");
+    time.classList.add("history-time");
+    time.innerText = DateFormat.format.prettyDate(new Date(item["time"]));
+
+    const long = document.createElement("p");
+    long.classList.add("long-link");
+    long.innerText = item["url"];
+
+    const short = document.createElement("input");
+    short.classList.add("short-link");
+    short.type = "text";
+    short.id = `short-link_${item["shortUrl"]}`;
+    short.value = item["shortUrl"];
+
+    itemElement.append(time, long, short);
+    return itemElement;
+}
+
+function renderHistory() {
+    var webAppHistory = JSON.parse(window.localStorage.history || "[]");
+    var extensionHistory = JSON.parse(
+        window.localStorage.extensionHistory || "[]"
+    );
+    var combinedHistory = [...webAppHistory, ...extensionHistory];
+    if (combinedHistory.length > 0) {
+        historySection.hidden = false;
+        historySection.nextElementSibling.hidden = false;
+    }
+    historySectionList.innerHTML = "";
+    combinedHistory.sort((a, b) => {
+        const key = "time";
+        if (a[key] < b[key]) return 1;
+        if (a[key] > b[key]) return -1;
+        return 0;
+    });
+    for (var item of combinedHistory) {
+        historySectionList.append(createHistoryItem(item));
+    }
+}
+
+async function saveToHistory(url, shortUrl, title) {
+    var webAppHistory = JSON.parse(window.localStorage.history || "[]");
+    webAppHistory.unshift({
+        url,
+        shortUrl,
+        title,
+        time: Date.now(),
+    });
+    window.localStorage.history = JSON.stringify(webAppHistory);
+}
+
+async function convert() {
+    if (converting) return;
+    converting = true;
+    convertButton.disabled = true;
+    copyButton.disabled = true;
+    let url = inputField.value;
+    outputField.value = "Loading";
+    outputField.value = await convertURL(url, "");
+    convertButton.disabled = false;
+    copyButton.disabled = false;
+    converting = false;
+}
+
+var clipboard = new ClipboardJS(copyButton);
+
+async function convertURL(link, title) {
+    try {
+        let response = await fetch(
+            "https://url.feli.page/api/urlShortener?apiKey=feli-page",
+            {
+                method: "GET",
+                headers: {
+                    "domain-uri-prefix": "https://url.feli.page/link",
+                    "request-link": link,
+                    "request-type": "UNGUESSABLE",
+                    "social-title": title,
+                    "social-description": link,
+                },
+            }
+        );
+        let json = await response.json();
+        console.log(json);
+        if (json["data"] && json["data"]["shortLink"]) {
+            await saveToHistory(link, json["data"]["shortLink"], "");
+            await renderHistory();
+            return json["data"]["shortLink"];
+        }
+    } catch (error) {}
+    return "Error";
+}
+
+convertButton.addEventListener("click", convert);
+renderHistory();
